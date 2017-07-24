e0afa886b816883180349c5cdc2ff3852c298cef
Alex Gian <agian@pint.com>
2017-07-24 10:29:37 -0700

    gitignore

--- app/server.py
+++ app/server.py
@@ -5,11 +5,16 @@ import sb_cl_nlp_for_app as spam
 app = Flask(__name__)
 app.debug = True
 
-@app.route('/', methods=['POST'])
-def hello_world():
-    msg = request.form['msg']
-    res = spam.checkSpam(msg)
-    return jsonify(result=int(res))
+@app.route('/', methods=['GET'])
+def healthcheck():
+    return 'It works!'
 
+@app.route('/', methods=['POST'])
+def checkSpam():
+    if request.form["msg"]:
+        msg = request.form['msg']
+        res = spam.checkSpam(msg)
+        return jsonify(result=int(res))
+    return ''
 if __name__ == '__main__':
     app.run()
